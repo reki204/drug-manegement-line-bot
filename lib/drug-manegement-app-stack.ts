4d5795c36067a6f89d35195eac4fea1ff41f4eba
Onda Reki <rekionda0524@ezweb.ne.jp>
2025-03-09 21:44:24 +0900

    ssmパラメーター取得

--- lib/drug-manegement-app-stack.ts
+++ lib/drug-manegement-app-stack.ts
@@ -4,15 +4,30 @@ import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigw from "aws-cdk-lib/aws-apigateway";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as ssm from "aws-cdk-lib/aws-ssm";
 
 export class DrugManegementAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const channelAccessToken = ssm.StringParameter.valueForStringParameter(
+      this,
+      "channel-access-token"
+    );
+
+    const channelSecret = ssm.StringParameter.valueForStringParameter(
+      this,
+      "channel-secret"
+    );
+
     const fn = new NodejsFunction(this, "lambda", {
       entry: "lambda/index.ts",
       handler: "handler",
       runtime: lambda.Runtime.NODEJS_20_X,
+      environment: {
+        CHANNEL_ACCESS_TOKEN: channelAccessToken,
+        CHANNEL_SECRET: channelSecret,
+      },
     });
 
     fn.addFunctionUrl({
@@ -37,8 +52,11 @@ export class DrugManegementAppStack extends cdk.Stack {
       this,
       "MedicationHistory",
       {
-        partitionKey: { name: "userId", type: dynamodb.AttributeType.STRING },
-        sortKey: { name: "medicationId", type: dynamodb.AttributeType.STRING },
+        partitionKey: {
+          name: "historyId",
+          type: dynamodb.AttributeType.STRING,
+        },
+        sortKey: { name: "takenTime", type: dynamodb.AttributeType.STRING },
         tableName: "MedicationHistory",
         billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
         removalPolicy: cdk.RemovalPolicy.DESTROY,
