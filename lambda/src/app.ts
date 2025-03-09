a4e8c14e30c8861b579925a738e83de1aaf17211
Onda Reki <rekionda0524@ezweb.ne.jp>
2025-03-10 00:24:17 +0900

    honoリファクタリング

--- lambda/src/app.ts
+++ lambda/src/app.ts
@@ -1,10 +1,31 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
-import medications from "../src/api/medications";
+import medicationRoutes from "./routes/medicationsRoutes";
+import { lineWebhookMiddleware } from "./line/webhookMiddleware";
+import { handleTextMessage } from "./handlers/messageHandler";
+import { createLineClient } from "./line/client";
+import type { WebhookEvent } from "@line/bot-sdk";
 
 const app = new Hono();
 
 app.use("*", logger());
-app.route("/", medications);
+app.use("/webhook", lineWebhookMiddleware);
+
+app.get("/", (c) => c.text("Hello Hono!"));
+app.post("/webhook", async (c) => {
+  const lineClient = createLineClient(c);
+
+  const events: WebhookEvent[] = (await c.req.json()).events;
+
+  for (const event of events) {
+    if (event.type === "message" && event.message.type === "text") {
+      await handleTextMessage(event, lineClient);
+    }
+  }
+
+  return c.json({ message: "Webhook received" });
+});
+
+app.route("/", medicationRoutes);
 
 export default app;
